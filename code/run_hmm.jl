503ae5a57da00af0891a2e1e3b55b8f4eae7ee30
Joe Saia <joe5saia@gmail.com>
2019-07-02 12:05:48 -0400

    initial commit

---
+++ code/run_hmm.jl
@@ -0,0 +1,47 @@
+###############################################################################
+#= 
+Program: run_hmm.jl 
+Author: Joe Saia
+Date: July 2019
+Info: Runs code to estimate hidden markov models with Gibbs sampling as 
+defined in the module hmc.jl for a range of horizions and saves results
+Input: Takes a Stata file with the inflation series as an Input
+Output: Writes several csvs
+=#
+###############################################################################
+## Supply the data series as a command line argument 
+if length(ARGS) == 0
+    @error "Must supply either \"official\" or \"alter\" as command line argument e.g. \n\tjulia --project code/run_hmm.jl official"
+    exit(1)
+elseif ARGS[1] == "official"
+    series = :offic_inf
+    filesuffix = "official"
+    println("Using official inflation")
+elseif ARGS[1] == "alter"
+    series = :alter_inf
+    filesuffix = "alter"
+    println("Using alternative inflation")
+else 
+    @error "Must supply either \"official\" or \"alter\" as command line argument e.g. \n\tjulia --project code/run_hmm.jl official"
+    exit(2)
+end
+
+using StatFiles
+using DataFrames
+using Dates
+include("src/hmc.jl")
+using .Hmc
+
+
+rawdata = DataFrame(load("data/raw/sgs_data.dta"))
+rawdata[:date] = makedate.(rawdata[:date])
+startindex = findfirst(isequal(Date(2017, 11)), rawdata[:date])
+endindex = findfirst(isequal(Date(2017, 12)), rawdata[:date])
+
+results = sampleAndForecastAll(Vector{Float64}(rawdata[series]),
+    Vector{Date}(rawdata[:date]), 1:endindex, 1:12, startindex:endindex;
+    D = 3, burnin = 1_000, Nrun = 1_000, initialburn = 1_000, initialNrun = 1)
+saveresults(results, filesuffix, "data/output/$(filesuffix)/") 
+
+stateresults = smoothStates(Vector{Float64}(rawdata[series]), rawdata[:date], 1:endindex; D = 3, burnin = 1_000, Nrun = 1_000)
+savesmoothresults(stateresults, "data/output/$(filesuffix)/")
---
+++ test/hmm_test.jl
@@ -0,0 +1,23 @@
+using Dates
+using Test
+include("src/hmc.jl")
+using .Hmc
+
+#Number of observations
+T = 5000
+# State Transition Matrix
+A99 = [0.25 0.75;
+       0.60 0.40]
+# means and variances
+μ99 = [-5.0, 4.0]
+σ99 = [1.0, 0.50]
+
+Y99, X99 = generateData(A99, μ99, σ99, T, 2)
+dates = range(Date(1970, 1, 1), step = Dates.Month(1), length = T)
+
+results = Hmc.sampleAndForecastAll(Y99, dates, 1:T - 20, 0:2, T - 20:T - 20;
+                    D = 2, burnin = 1_000, Nrun = 10_000,
+                    initialburn = 10_000, initialNrun = 1)
+
+@test all(isapprox.(results[2][2:end], μ99, atol=0.1))
+@test all(isapprox.(results[3][2:end], σ99, atol=0.1))
