305083da67f9f1c1c7b78db850a58f87f806ac28
Joseph Saia <js4956@bake.cm.cluster>
2019-07-02 16:18:16 -0400

    add batch scripts for slurm

---
+++ alt_estimation.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+#SBATCH --account=sscc # The account name for the job.
+#SBATCH --job-name=runestiamtion_alt # The job name.
+#SBATCH -c 1 # The number of cpu cores to use.
+#SBATCH --time=10:00:00 # The time the job will take to run.
+#SBATCH --mem-per-cpu=5gb # The memory the job will use per cpu core.
+#SBATCH --output=output/slurm/slurm_alt.txt
+
+
+module load julia
+
+julia --project=. code/run_hmm.jl alter
+ 
+
+#End of script
