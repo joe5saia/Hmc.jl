64890e8d05d1bd1ec370ef86e1af9c1e8a491dc8
Joe Saia <joe5saia@gmail.com>
2019-07-05 16:54:42 -0400

    updated slurm scripts

--- base_estimation.sh
+++ base_estimation.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 #
 #SBATCH --account=sscc # The account name for the job.
-#SBATCH --job-name=runestiamtion_official # The job name.
-#SBATCH -c 1 # The number of cpu cores to use.
-#SBATCH --time=10:00:00 # The time the job will take to run.
-#SBATCH --mem-per-cpu=5gb # The memory the job will use per cpu core.
-#SBATCH --output=output/slurm/slurm_offical.txt
+#SBATCH --job-name=official_estimate # The job name.
+#SBATCH -N 1 # The number of nodes to use.
+#SBATCH --time=05-0:00:00 # The time the job will take to run.
+#SBATCH --mem=100G # The memory the job will use per cpu core.
+#SBATCH --output=output/slurm/slurm_official.txt
 #SBATCH --chdir=/moto/sscc/projects/biasedexpectations
 
 
 module load julia
 
-julia --project=. run_hmm.jl official
+julia --project=. -p 24 run_hmm.jl official
+
