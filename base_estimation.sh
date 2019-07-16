9b5d7eec0e77d948afedeca9b19b264c46209a1f
Joe Saia <joe5saia@gmail.com>
2019-07-16 15:19:48 -0400

    touch up code to get ready to do actual run

--- base_estimation.sh
+++ base_estimation.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-#
+
 #SBATCH --account=sscc # The account name for the job.
 #SBATCH --job-name=official_estimate # The job name.
-#SBATCH -N 1 # The number of nodes to use.
-#SBATCH --time=05-0:00:00 # The time the job will take to run.
-#SBATCH --mem=100G # The memory the job will use per cpu core.
-#SBATCH --output=output/slurm/slurm_official.txt
+#SBATCH --array=121-122 
+#SBATCH --time=1-0:00:00 # The time the job will take to run.
+#SBATCH -n 1 # one cpu core
+#SBATCH --mem=20G # The memory the job will use per cpu core.
+#SBATCH --output=output/slurm/slurm_official_%a.txt
 #SBATCH --chdir=/moto/sscc/projects/biasedexpectations
 
 
 module load julia
 
-julia --project=. -p 24 run_hmm.jl official
-julia --project=. aggregate.jl official
+julia --project=. /moto/sscc/projects/biasedexpectations/code/run_hmm.jl official ${SLURM_ARRAY_TASK_ID}
