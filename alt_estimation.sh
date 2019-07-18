36829ffe1cae42254d49f9852faa2d4937122589
Joe Saia <joe5saia@gmail.com>
2019-07-18 13:38:41 -0400

    updated run scripts

--- alt_estimation.sh
+++ alt_estimation.sh
@@ -2,14 +2,14 @@
 
 #SBATCH --account=sscc # The account name for the job.
 #SBATCH --job-name=alt_estimate # The job name.
-#SBATCH --array=121-546 
-#SBATCH --time=1-0:00:00 # The time the job will take to run.
+#SBATCH --array=121-576 
+#SBATCH --time=4:00:00 # The time the job will take to run.
 #SBATCH -n 1 # one cpu core
-#SBATCH --mem=20G # The memory the job will use per cpu core.
-#SBATCH --output=output/slurm/slurm_official_%a.txt
+#SBATCH --mem=5G # The memory the job will use per cpu core.
+#SBATCH --output=output/slurm/slurm_alt_%a.txt
 #SBATCH --chdir=/moto/sscc/projects/biasedexpectations
 
 
 module load julia
 
-julia --project=. /moto/sscc/projects/biasedexpectations/code/run_hmm.jl alter %a
+julia --project=. /moto/sscc/projects/biasedexpectations/code/run_hmm.jl alter ${SLURM_ARRAY_TASK_ID}
