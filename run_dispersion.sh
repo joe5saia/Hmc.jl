e4d6d05612b37d003f0c52f832cbfc0019c66d79
Joe Saia <joe5saia@gmail.com>
2019-07-22 12:53:38 -0400

    added shell script to run dispersion code

---
+++ run_dispersion.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#SBATCH --account=sscc # The account name for the job.
+#SBATCH --job-name=aggregate # The job name.
+#SBATCH -c 1 # The number of cpus to use.
+#SBATCH --time=5:00:00 # The time the job will take to run.
+#SBATCH --mem=10G # The memory the job will use per cpu core.
+#SBATCH --chdir=/moto/sscc/projects/biasedexpectations
+#SBATCH --output=output/slurm/slurm_dispersion.txt
+
+
+module load julia
+
+julia --project=. code/dispersion.jl
+
