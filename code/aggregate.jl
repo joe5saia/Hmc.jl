f3fd4e068daad3f837959ab16e0973ccded30ab9
Joe Saia <joe5saia@gmail.com>
2019-07-26 11:09:17 -0400

    changed parameters for testing
    testing

--- code/aggregate.jl
+++ code/aggregate.jl
@@ -25,9 +25,9 @@ Pkg.activate(root_dir)
 push!(LOAD_PATH, joinpath(root_dir, "src"))
 using Hmc
 
-for s in ["official", "alter"]
+for s in ["official"]
     Hmc.runaggregate(joinpath(root_dir, "data/output/$(s)"))
-    for n in ["0.1", "1.0", "3.0"], m in ["1"]
+    for n in ["0.01"], m in ["12"]
         if ispath(joinpath(root_dir, "data/output/signals_$(s)_noise_$(n)_len_$(m)"))
             Hmc.runaggregate(joinpath(root_dir, "data/output/signals_$(s)_noise_$(n)_len_$(m)"))
         else
--- code/run_hmm.jl
+++ code/run_hmm.jl
@@ -106,7 +106,7 @@ else
         signalNrun = 3_000,
         signalLen = 1,
         noise = 0.0,
-        noiseSamples = 300,
+        noiseSamples = 30,
         σsignal = 0.0,
         savenosignal= false,
         series = series,
@@ -127,21 +127,21 @@ end
 
 
 ## Estimate model for each horizon we want with signals for different noise levels
-opt.signalLen=1
-opt.savenosignal=false
-for noiselevel in [0.1 1.0 3.0]
-    println("Running noise = $(noiselevel) sample")
-    opt.noise=noiselevel
-    p = "data/output/signals_$(opt.series)_noise_$(opt.noise)_len_$(opt.signalLen)"
-    !ispath(p) && mkpath(p)
-    Random.seed!(opt.seed)
-    samples = Hmc.estimatesignals!(opt)
-    Hmc.saveresults(samples, opt; hassignals = true)
-end
+#opt.signalLen=1
+#opt.savenosignal=false
+#for noiselevel in [0.1 1.0 3.0]
+#    println("Running noise = $(noiselevel) sample")
+#    opt.noise=noiselevel
+#    p = "data/output/signals_$(opt.series)_noise_$(opt.noise)_len_$(opt.signalLen)"
+#    !ispath(p) && mkpath(p)
+#    Random.seed!(opt.seed)
+#    samples = Hmc.estimatesignals!(opt)
+#    Hmc.saveresults(samples, opt; hassignals = true)
+#end
 
 opt.signalLen=12
 opt.savenosignal=false
-for noiselevel in [0.1 1.0 3.0]
+for noiselevel in [0.01]
     println("Running noise = $(noiselevel) sample")
     opt.noise=noiselevel
     p = "data/output/signals_$(opt.series)_noise_$(opt.noise)_len_$(opt.signalLen)"
